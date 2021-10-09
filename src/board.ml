ab6afb79bf5498450a4fb63c0a03b1fd765d6cba
Seokyoung Hong <sh845@cornell.edu>
2021-10-09 13:01:41 -0400

    Added specs to board, starting test for correctness

--- src/board.ml
+++ src/board.ml
@@ -1,47 +1,50 @@
 open Yojson.Basic.Util 
-(** Translate json to a chess board. 
-Two dimensional list for each of the positions. *)
 
+(** Represents a single chess board square*)
 type position = {
   id : int * int; 
   obstacle : string;
   color : string;
 }
 
-(* Keeping the # of rows and columns, as well 
-how to access them general in case we want to implement 
-piece functions more flexibly *)
+(** Keeps track of dimensions of board, and positions as 2D list. *)
 type board = {
   number_of_rows : int;
   number_of_columns : int;
   positions : position list list;
 }
 
-(* there must be a prettier way to write this
-but I forgot how to pattern match against lists*)
-let convert_yojson_tuple = function 
+(** Converts a yojson list to a pair 
+Requires: the json list you pass in must have two elements 
+Note: there must be a prettier way to write this but I forgot how to pattern match against lists*)
+let yojsonlist_to_tuple = function 
   | x :: y :: [] -> (to_int x, to_int y)
   | _  -> failwith "Nonreal position"
 
+(** [position_from_json json] is a representation of am individual square. *)
 let position_from_json json = 
   {
-      id = json |> member "id" |> to_list |> convert_yojson_tuple; 
+      id = json |> member "id" |> to_list |> yojsonlist_to_tuple; 
       obstacle = json |> member "obstacle" |> to_string;
       color = json |> member "color" |> to_string;
   }
 
+(** [positions1D json] is the list of all the squares on the board.
+Example: 2x2 board will store positions w/ id [(1,1), (1,2), (2,1), (2,2)]*)
 let positions1D json = 
   json |> member "positions" |> to_list |> List.map position_from_json 
 
+(** [dimensionsTuple json] is the dimensions of the board (row, col)*)
 let dimensionsTuple json = 
-  json |> member "name" |> to_list |> convert_yojson_tuple
+  json |> member "name" |> to_list |> yojsonlist_to_tuple
 
-
-let rec positions2D pos_lst c x acc1 acc2 = 
+(** [positions2D pos_lst c x rowacc boardacc] helps construct the positions -
+implemented as a 2D list comprised of position types*)
+let rec positions2D pos_lst c x rowacc boardacc = 
   match pos_lst with 
   | [] ->  failwith "you moron. you imbecile. you screwed up bigtime."
-  | h :: _ when x = c -> positions2D pos_lst c 1 [] ((h :: acc1) :: acc2)
-  | h :: _ -> positions2D pos_lst c (x + 1) (h :: acc1) (acc2)
+  | h :: _ when x = c -> positions2D pos_lst c 1 [] ((h :: rowacc) :: boardacc)
+  | h :: _ -> positions2D pos_lst c (x + 1) (h :: rowacc) (boardacc)
 
 let board_from_json json = 
   {
@@ -50,5 +53,11 @@ let board_from_json json =
     positions = positions2D (positions1D json) (snd (dimensionsTuple json) ) 1 [] []
 } 
 
-let testplz input = input + 2 
+(** TO DO: Implement nth row, nth column, finding positions
+to test if abstraction is correct 
+let n_row n board = 
+
+let n_col n board = 
+
+  *) 
   
--- src/board.mli
+++ src/board.mli
@@ -1,5 +1,6 @@
+(** Abstract type of values representing a chess board.*)
 type board 
 
-val testplz : int -> int 
-
+(** [board_from_json json] is the chess board that [json] represents.contents
+Requires: [json] is a rectangular representation.*)
 val board_from_json : Yojson.Basic.t -> board 
