5ec7b6c62cd397776c0b767be3dd64ebcbd2ca1d
Seokyoung Hong <seokyoung99@gmail.com>
2021-10-12 17:57:21 -0400

    Revert "Lez"

---
+++ _build/default/src/board.mli
@@ -0,0 +1,28 @@
+(** Abstract type of values representing a chess board.
+NOTE: REPLACE NAME TO t once we are familiar with it*)
+type board 
+
+(**Abstract type of values representing a chess square*)
+type position
+
+(** Where the chess square location is*)
+type id = int * int
+
+(** [board_from_json json] is the chess board that [json] represents.contents
+Requires: [json] is a rectangular representation.*)
+val board_from_json : Yojson.Basic.t -> board 
+
+(** [n_row n board] is the nth row of the chess board
+Requires: [n] >= 1*)
+val n_row : int -> board -> position list  
+
+(** [n_row n board] is the nth column of the chess board
+Requires: [n] >= 1*)
+val n_col : int -> board -> position list 
+
+(** [id_lst id_pos_lst] produces a list of ids from a list of positions *)
+val id_pos_lst : position list -> id list 
+
+(** [id_board board]* produces a list of id lists from a board, essentially
+mapping out the whole board *)
+val id_board : board -> id list list 
---
+++ _build/default/src/jsongen.ml
@@ -0,0 +1,48 @@
+(** Helper that kills alex the individual positionss *)
+let string_json r c white = 
+  
+  (*can change to list, pair if needed*)
+  let id_as_string = string_of_int r ^ ", " ^ string_of_int c in
+  let black_or_white = if white then "white" else "black" in
+  
+  "{
+      'id' : [" ^ id_as_string ^ "],
+      'obstacle': 'none',
+      'color' : '" ^ black_or_white ^ "'
+      },"
+
+(** [create_board n r c max_col] Creates an [n] x [n] chess board json, starting 
+at position ([r],[c]) with the template:
+  {
+    'name': [r, c], 
+    'positions': [
+      {
+          'id': (rxc),
+          'obstacle': 'none or some'
+          'color': 'white or black'
+      },
+      etc.
+    ]
+    }
+  Note: single string markers will be replaced by double quotes. *) 
+let rec create_board height width r c sqrn = 
+
+  if sqrn = 0 then "" else 
+
+  let is_even = (r + c) mod 2 = 0 in 
+
+  if c = width then string_json r c is_even  ^ create_board height width (r+1) 1 (sqrn-1)
+  else string_json r c is_even ^ create_board height width r (c+1) (sqrn - 1)
+
+(** Obviously prints out the board*)
+let print_board height width r c = 
+
+  let num_positions = height * width in 
+  let positions = create_board height width r c num_positions in 
+
+  let final_string =  " { 'name': '" ^ string_of_int height ^ "x" ^ string_of_int width ^ "', 
+  'positions': [ " ^ positions ^ " ]}" in
+
+
+
+  print_endline (final_string)
