e3a41eb53cf3985ed90539e2e731d151257482b6
Seokyoung Hong <sh845@cornell.edu>
2021-10-23 15:22:51 -0400

    Changed jsongen so it outputs string, supports gui

--- src/board.ml
+++ src/board.ml
@@ -94,7 +94,7 @@ let num_rows (board : board) : int =
   board.number_of_rows
 
 let piece_pos_lst (pos_lst : position list) = 
-    List.map (fun x -> get_name (x.piece)) pos_lst
+    List.map (fun x -> get_name (x.piece)) pos_lst 
 
 let piece_board board = 
   List.map (fun x -> piece_pos_lst x) board.positions
@@ -138,4 +138,9 @@ let get_obstacle (board:board) (id:id):string =
   | [] -> failwith "Invalid id given"
   | h::t -> if (h.id=id) then h.obstacle else get_helper t id get_obstacle_helper
 
-(** Finding position to test if abstraction is correct *) 
+(* TODO: Doc comment*)
+
+(*
+let put_piece id piece board =
+  board.positions  *)
+
