ddb82b549084e8c7349afae6048cc668ec0c2e38
Henry Chen <hc659@cornell.edu>
2021-11-03 21:16:13 -0400

    Committing changes before pulling again

--- gui/gui.ml
+++ gui/gui.ml
@@ -2,6 +2,11 @@ open Raylib
 open Raygui
 open Chess
 open Board
+open Loaded_images
+
+(** This code is temporarility here which allows the code to be built
+    and rendered.*)
+let image_pawn = black_pawn_image
 
 (*An object of type tile which contains a rectangle and a position*)
 type tile = {
---
+++ gui/loaded_images.ml
@@ -0,0 +1,21 @@
+open Raylib
+
+let black_pawn_image = load_image "./assets/black_pawn.png"
+
+let black_knight_image = load_image "./assets/black_knight.png"
+
+let black_king_image = load_image "./assets/black_king.png"
+
+let black_castle_image = load_image "./assets/black_castle.png"
+
+let black_bishop_image = load_image "./assets/black_bishop.png"
+
+let white_pawn_image = load_image "./assets/white_pawn.png"
+
+let white_knight_image = load_image "./assets/white_knight.png"
+
+let white_king_image = load_image "./assets/white_king.png"
+
+let white_castle_image = load_image "./assets/white_castle.png"
+
+let white_bishop_image = load_image "./assets/white_bishop.png"
